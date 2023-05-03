940eab780a9b93a0f68965449f02565ad9e50549
afkanpour <71746063+afkanpour@users.noreply.github.com>
2023-05-03 15:50:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(
-    name="guided-diffusion",
+    name="rcdm",
     py_modules=["guided_diffusion"],
     install_requires=["blobfile>=1.0.5", "torch", "tqdm", "numpy", "torchvision", "faiss-gpu"],
 )
